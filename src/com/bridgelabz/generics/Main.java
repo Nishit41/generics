8e8ef490ebb2defd325192b9c6bec6ef0ed911ac
Nishit <nishitranjan41@gmail.com>
2022-07-30 08:41:06 +0530

    this commit is for float

--- src/com/bridgelabz/generics/Main.java
+++ src/com/bridgelabz/generics/Main.java
@@ -9,5 +9,12 @@ public class Main {
         maxInt.maxvalue(5,6,4);
         System.out.println("Test case 3");
         maxInt.maxvalue(5,6,8);
+        MaxFloat maxFloat = new MaxFloat();
+        System.out.println("Test case 1");
+        maxFloat.maxvalue(7.8F,5.5F,6.3F);
+        System.out.println("Test case 2");
+        maxFloat.maxvalue(5.2F,6.3F,4.5F);
+        System.out.println("Test case 3");
+        maxFloat.maxvalue(5.2F,6.2F,8.6F);
     }
 }
